d1d4c980402b02ed793a1b1809fb16d9883ffdae
Mehdi Benamar <mistahbe213@gmail.com>
2023-08-29 16:03:36 +0200

    add function getAllEvents

--- public/index.php
+++ public/index.php
@@ -1,14 +1,18 @@
 <?php
   use Calendar\Month;
+  use Calendar\Event;
+
   require "../vendor/autoload.php";
   
   try {
     $initialDate = new Month($_GET["month"] ?? null, $_GET["year"] ?? null);
   } catch (\Exception $e) {
-      echo $e->getMessage();
+    echo $e->getMessage();
   }
-
   $lastMonday = $initialDate->getStartDay()->modify("last monday");
+  
+  $event = new Event();
+  $event->getAllEvents($initialDate->getStartDay(), (clone $initialDate)->getStartDay()->modify("+1 month -1 day"));
 ?>
 
 <!DOCTYPE html>
@@ -31,12 +35,14 @@
   <table class="table__calendar">
     <?php for($semaine = 0; $semaine < $initialDate->getWeeks(); $semaine++): ?>
       <tr class="table__week table__<?= $initialDate->getWeeks() ?>weeks">
-        <?php foreach($initialDate->days as $k => $day): ?>
+        <?php foreach($initialDate->days as $k => $day):
+              $addDays = (clone $lastMonday)->modify("+" . $k + ($semaine * 7) . " day");
+        ?>
           <td>
             <?php if($semaine === 0): ?>
               <div class="calendar__day"><?= $day ?></div>
             <?php endif ?>
-            <div class="calendar_number_day <?= $initialDate->isNotSameDate((clone $lastMonday)->modify("+" . $k + ($semaine * 7) . " day")) ? "calendar_not_same_date" : '' ?>"><?= (clone $lastMonday)->modify("+" . $k + ($semaine * 7) . " day")->format("d"); ?></div>
+            <div class="calendar_number_day <?= $initialDate->isNotSameDate($addDays) ? "calendar_not_same_date" : '' ?>"><?= $addDays->format("d"); ?></div>
           </td>
         <?php endforeach ?>
       </tr>
--- src/Event.php
+++ src/Event.php
@@ -1,18 +1,29 @@
 <?php
 
-namespace Calender;
+namespace Calendar;
 
 use \PDO;
+use \DateTime;
 
 class Event {
 
+  private \PDO $pdo;
+
     public function __construct()
     {
-      $pdo = new PDO("sqlite:../database/db.sql", null, null, [
+      $pdo = new PDO("sqlite:../database/db.sqlite", null, null, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
       ]);
 
-     return $pdo;
+     return $this->pdo = $pdo;
+    }
+
+    public function getAllEvents(DateTime $start, DateTime $end): ?array
+    {
+      $start = $start->getTimestamp();
+      $end = $end->getTimestamp();
+      $query = $this->pdo->query("SELECT * FROM events WHERE start BETWEEN $start AND $end");
+      return $query->fetchAll();
     }
 }
