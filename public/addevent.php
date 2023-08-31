7b217f7b2ecca7f3b417af3c94acf6cd50f6793b
Mehdi Benamar <mistahbe213@gmail.com>
2023-08-31 15:27:53 +0200

    style the form event

--- public/addevent.php
+++ public/addevent.php
@@ -1,9 +1,11 @@
 <?php
+  require "../vendor/autoload.php";
   $title = "Créer un évènement";
+  dump($_POST);
   require "../views/header.php";
 ?>
-  <section class="section_form">
-    <h2>Ajouter un évènement</h2>
+  <section class="section_event">
+    <h2>Ajouter un Évènement</h2>
 
     <form action="" method="post">
       <div class="form_date_start">
@@ -20,7 +22,9 @@
       <div class="form_date_description">
         <textarea name="description" placeholder="Description de l'évènement"></textarea>
       </div>
+      <button type="submit">Ajouter</button>
     </form>
   </section>
+
 
 <?php require "../views/footer.php"; ?>
