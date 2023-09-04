c4996d37473fe5536849fd47cd3bb82cf6f3fcf9
Mehdi Benamar <mistahbe213@gmail.com>
2023-09-04 15:05:42 +0200

    add error condition html

--- public/addevent.php
+++ public/addevent.php
@@ -1,16 +1,17 @@
 <?php
   require "../vendor/autoload.php";
+  $title = "Créer un évènement";
+
   use Calendar\Validator;
   $validate = (new Validator())->validates($_POST);
-
+  $errors = [];
   if(empty($validate)){
 
   }else{
-    dump($validate);
+    $errors = $validate;
   }
   
   
-  $title = "Créer un évènement";
   require "../views/header.php";
 ?>
   <section class="section_event">
@@ -19,13 +20,24 @@
     <form action="" method="post">
       <div class="form_date_start">
         <input type="date" name="start_date" value="">
+        <?php if(isset($errors["start_date"])): ?>
+          <small><?= $errors["start_date"] ?></small>
+        <?php endif ?>
       </div>
       <div class="form_time">
-        <input type="time" name="start_time" value="">
+        <div>
+          <input type="time" name="start_time" value=""><br>
+          <?php if(isset($errors["start_time"])): ?>
+            <small><?= $errors["start_time"] ?></small>
+          <?php endif ?>
+        </div>
         <input type="time" name="end_time" value="">
       </div>
       <div class="form_date_title">
         <input type="text" name="title" placeholder="Titre de l'évènement">
+        <?php if(isset($errors["title"])): ?>
+          <small><?= $errors["title"] ?></small>
+        <?php endif ?>
       </div>
       <div class="form_date_description">
         <textarea name="description" placeholder="Description de l'évènement"></textarea>
